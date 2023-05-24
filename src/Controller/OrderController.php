c768c5aa5f61cdc62501e38237383e405fc378bb
Chou VANG <lesolo91@hotmail.com>
2023-05-24 17:09:37 +0200

    debug frais de transport

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use App\Entity\Transporteur;
 use App\Entity\User;
 use App\Form\OrderType;
+use App\Form\TransporteurType;
 use App\Repository\AdressesRepository;
 use App\Repository\PhotosRepository;
 use App\Repository\ProductRepository;
@@ -25,7 +26,7 @@ class OrderController extends AbstractController
     /**
      * @Route("/order/view", name="order_index")
      */
-    public function index(CartService $cartService, AdressesRepository $adressesRepository, TransporteurRepository $transporteurRepository): Response
+    public function index(CartService $cartService, AdressesRepository $adressesRepository, TransporteurRepository $transporteurRepository, Request $request): Response
     {
         if(!$this->getUser())
         {
@@ -36,18 +37,29 @@ class OrderController extends AbstractController
         $form = $this->createForm(OrderType::class, null, [
             'user' => $user,
         ]);
+        $transportForm = $this->createForm(TransporteurType::class);
+        $transportForm->handleRequest($request);
+        $transporteur = 0;
 
+        if ($transportForm->isSubmitted() && $transportForm->isValid()) {
+            $transporteur = $transportForm->get('transporteur')->getData();
+            $this->get('session')->set('transporteur', $transporteur);
+        }
+        
         $panierWithDatas = $cartService->show();
         $total = $cartService->getTotalPrice();
+        $totalInclShipping = $total + $transporteur;
+
         $totalQuantity = $cartService->getTotalQuantity();
 
         $deliverys = $transporteurRepository->findAll();
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
             'form' => $form->createView(),
+            'transportForm' => $transportForm->createView(),
             'items' => $panierWithDatas,
             'total' => $total,
-            // 'totalInclShipping' => $totalInclShipping,
+            'totalInclShipping' => $totalInclShipping,
             'totalQuantity' => $totalQuantity,
             'user' => $user,
             'adresses' => $adresses,
@@ -57,29 +69,18 @@ class OrderController extends AbstractController
     }
 
     /**
-     * @Route("/order/total", name="order_total")
-     */
-    public function getTotalPriceIncDelivery() {
-        // $totalInclShipping =
-        dd();
-    }
-
-    /**
      * @Route("/order/create", name="order_create")
      */
-    public function create(CartService $cartService)
+    public function create(CartService $cartService, Request $request)
     {
-
-        $montant=$cartService->getTotalPrice()*100;
-
         // clé secrete pour que stripe me reconnaisse
         $stripeSecretKey="sk_test_51N7BLTLSTvAVXmvAMIVodTwS5yNjyYebx3cdcXYH9CGn40AcMwpVRNSgi30nbQ1aYIdM4IKtM9QOzUn22V6RlI0v00pxiDAkKs";
         Stripe::setApiKey($stripeSecretKey);
-  
+        
         // on définit le protocol de connexion http ou https
         // avec les variable global PHP de sorte de pouvoir gérer
         // tout les environnements possible
-
+        
         if (isset($_SERVER['HTTPS'])){
             $protocol="https://";
         } 
@@ -90,13 +91,17 @@ class OrderController extends AbstractController
         $serveur=$_SERVER['SERVER_NAME'];
         $YOUR_DOMAIN=$protocol.$serveur;
 
+        $transporteur = $this->get('session')->get('transporteur', 0)*100;
+
+        $montant=$cartService->getTotalPrice()*100;
+        //Je dois créer une variable pour recup le transporteur
         // on lance la communication avec stripe
         $checkout_session = \Stripe\Checkout\Session::create([
             'line_items' => [[
               # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
               'price_data' => [
                 'currency' => 'eur',
-                'unit_amount' => $montant,
+                'unit_amount' => $montant + $transporteur,
                 'product_data' => [
                   'name' => 'Paiement de votre panier'
                 ],
