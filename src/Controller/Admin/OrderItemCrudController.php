63ffe8f37109487444b5cbac204cf60924d6d513
Chou VANG <lesolo91@hotmail.com>
2023-06-01 20:59:03 +0200

    maj

--- src/Controller/Admin/OrderItemCrudController.php
+++ src/Controller/Admin/OrderItemCrudController.php
@@ -4,12 +4,6 @@ namespace App\Controller\Admin;
 
 use App\Entity\OrderItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class OrderItemCrudController extends AbstractCrudController
 {
@@ -18,16 +12,14 @@ class OrderItemCrudController extends AbstractCrudController
         return OrderItem::class;
     }
 
-    
+    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->onlyOnIndex(),
-            AssociationField::new('order_id'),
-            AssociationField::new('product_id'),
-            NumberField::new('quantity'),
-            MoneyField::new('price')->setCurrency('EUR'),
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
         ];
     }
-    
+    */
 }
