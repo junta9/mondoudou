dfd2cb184af4928ba6e981d5254d1ba31c05d7d9
Chou VANG <lesolo91@hotmail.com>
2023-06-08 23:23:23 +0200

    ajout factue pdf dans BO

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -3,7 +3,22 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
+use Dompdf\Adapter\PDFLib;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
 class OrderCrudController extends AbstractCrudController
 {
@@ -12,14 +27,31 @@ class OrderCrudController extends AbstractCrudController
         return Order::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            AssociationField::new('user_id')->hideOnForm()->hideOnIndex(),
+            IntegerField::new('total'),
+            ChoiceField::new('status')
+                ->setChoices([
+                    'en attente' => 'en attente',
+                    'en préparation' => 'en preparation',
+                    'commande pris en charge par le transporteur' => 'commande pris en charge par le transporteur'
+                ]),
+            TextField::new('delivery_address')->hideOnIndex(),
+            DateField::new('created_at')->hideOnForm(),
+            DateField::new('updated_at')->hideOnIndex(),
+            Field::new('invoice')->hideWhenUpdating()
+                ->setTemplatePath('pdf.html.twig'),
+            
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, "detail");
+    }
+    
 }
--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -6,6 +6,8 @@ use App\Repository\OrderRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @ORM\Entity(repositoryClass=OrderRepository::class)
@@ -186,5 +188,10 @@ class Order
         $this->delivery_price = $delivery_price;
 
         return $this;
+    }
+
+    public function getInvoice(){
+
+        return $this->getId();
     }
 }
